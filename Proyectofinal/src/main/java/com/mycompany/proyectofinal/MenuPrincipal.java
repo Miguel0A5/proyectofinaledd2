5e310ac53d5271de87e4fdacae7b9226b3ff9a49
Miguel Tress <u25103001@alumnos.univo.edu.mx>
2025-09-18 23:12:01 -0600

    Se agrega la opcion en el menu principal

--- Proyectofinal/src/main/java/com/mycompany/proyectofinal/MenuPrincipal.java
+++ Proyectofinal/src/main/java/com/mycompany/proyectofinal/MenuPrincipal.java
@@ -1,8 +1,8 @@
 package com.mycompany.proyectofinal;
+
 import java.util.*;
 
 public class MenuPrincipal {
-
 
     public void ejecutar() {
         Scanner entrada = new Scanner(System.in);
@@ -10,15 +10,21 @@ public class MenuPrincipal {
         do {
             System.out.println("\n--- MENÚ Principal ---");
             System.out.println("1.- Organizacíon secuencial");
+            System.out.println("2.- Busqueda secuencial");
+
             System.out.println("7.- Salir del programa");
-            //Aqui pongan sus opciones
+            // Aqui pongan sus opciones
             try {
                 opcion = Integer.parseInt(entrada.nextLine());
                 switch (opcion) {
-                    
-                    /*  Para inciar su porgrama aqui, escriban el nombre de su archivo, sesuigo de su nombre
-                        y al final le pondran new seguido dle nombre de su archivo y ();, despues ponen el 
-                        nombre del objeto . y el nombre de su método que ejecute todo*/
+
+                    /*
+                     * Para inciar su porgrama aqui, escriban el nombre de su archivo, sesuigo de su
+                     * nombre
+                     * y al final le pondran new seguido dle nombre de su archivo y ();, despues
+                     * ponen el
+                     * nombre del objeto . y el nombre de su método que ejecute todo
+                     */
                     case 1:
                         MiguelA5Gomez miguel = new MiguelA5Gomez();
                         miguel.ejecutar();
